c12ff531a8f2c42daf1693b604bd71990be7cdde
RICHARD GUTIERREZ CONDORI <rychard.gutierrez@gmail.com>
2020-01-24 14:35:15 -0400

    Creando prop de reac para enviar datos o funciones

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import Saludar from "./components/Saludar";
 import React from "react";
 import "./index.css";
 function App() {
-  const helloWorld = () => {
-    return "Hello World";
-  };
-  const numero = () => {
-    console.log(1);
+  const enviarSaludo = nombre => {
+    console.log(`Hello ${nombre}`);
   };
   return (
     <div className="App">
       <h1>Mi primer componente</h1>
-      <Saludar />
-      <br />
-      <Saludar />
+      <Saludar nombre="Ana" apellido="Montenegro" enviarSaludo={enviarSaludo} />
+      <Saludar
+        nombre="Richard"
+        apellido="Gutierrez"
+        enviarSaludo={enviarSaludo}
+      />
     </div>
   );
 }
